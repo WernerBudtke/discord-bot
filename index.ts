83ff837a58515430fd1f3d1866e2c624c5cb088b
Federico Werner Budtke <wernerbdev@gmail.com>
2024-10-31 01:13:33 -0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,7 @@
+const { Client, GuildMember, Intents } = require("discord.js");
+const { Player, QueryType } = require("discord-player");
+const config = require("./config.json");
+const client = new Client({
+    intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS]
+});
+client.login(config.token);
