d86970c8758038d4d83ef849c9b80ddfda6a032e
Federico Werner Budtke <wernerbdev@gmail.com>
2024-10-31 01:29:26 -0300

    Added husky folder

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
-const { Client, GuildMember, Intents } = require("discord.js");
-const { Player, QueryType } = require("discord-player");
-const config = require("./config.json");
+import { Client, GuildMember, Intents } from 'discord.js';
+import { Player, QueryType } from 'discord-player';
+import config from './config.json';
 const client = new Client({
-    intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS]
+  intents: [
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILDS,
+  ],
 });
 client.login(config.token);
